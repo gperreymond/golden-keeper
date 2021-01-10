92b3efda3581e000ce035215a6bf45f7fbce5de5
Gilles Perreymond <gperreymond@gmail.com>
2021-01-10 17:06:50 +0100

    refacto(database): remove timescaledb

--- timescaledb/create_database.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-npx sequelize db:create golden_keeper
--- timescaledb/generate_models.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-npx sequelize \
-    model:generate \
-    --name AwsEc2Instances \
-    --attributes \
-    time:date,instanceId:string,instanceType:string,region:string
